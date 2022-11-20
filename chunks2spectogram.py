0631bb3870fa2daecad47e780407b13102a6f9fa
Aaron <aaronctsang@gmail.com>
2022-11-20 16:41:22 +0800

    Splitter for all files in track directory Chunk2Spectogram for all files in split chunk directory Defined as functions for main

--- chunks2spectogram.py
+++ chunks2spectogram.py
@@ -1,18 +1,27 @@
 import librosa
-import urllib3
-
-y, sr = librosa.load(('/media/aaron/My Passport/FYP/a1.ogg'))
-# melspec = librosa.feature.melspectrogram(y=y, sr=sr)
-
-# Displays are built with matplotlib
+import librosa.display
 import matplotlib.pyplot as plt
+import numpy as np
+import os
 
-# Let's make plots pretty
-# import matplotlib.style as ms
-# ms.use('seaborn-muted')
+def chunks2spectogram():
+    destination_directory = "/media/aaron/My Passport/FYP/spectograms/"
+    input_directory = '/media/aaron/My Passport/FYP/chunks/'
 
-# Render figures interactively in the notebook
-# %matplotlib nbagg
+    counter = 1
+    for filename in os.listdir(input_directory):
+        f = os.path.join(input_directory, filename)
+        if os.path.isfile(f):
+            y, sr = librosa.load(f)
 
-# IPython gives us an audio widget for playback
-from IPython.display import Audio
+            n_mels = 128  # entire frequency spectrum split into 128
+            n_fft = 2048  # The amount of samples we are shifting after each fft (window size
+            hop_length = 512  # Short-time Fourier Transformation on our audio data
+
+            S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+            S_DB = librosa.power_to_db(S, ref=np.max)
+            librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='mel')
+            plt.axis('off')
+            plt.savefig(destination_directory + filename + '.png', bbox_inches='tight', pad_inches=0)
+            print(str(counter) + ", " + filename)
+            counter = counter + 1
--- splitter.py
+++ splitter.py
@@ -1,44 +1,46 @@
 from pydub import AudioSegment
 import os
 
-input_directory = "/media/aaron/My Passport/FYP/sample_tracks/"
-output_directory = "/media/aaron/My Passport/FYP/chunks/"
 
-for filename in os.listdir(input_directory):
-    f = os.path.join(input_directory, filename)
-    if os.path.isfile(f):
-        # print(f)
-        # print(filename)
+def splitter():
+    input_directory = "/media/aaron/My Passport/FYP/sample_tracks/"
+    output_directory = "/media/aaron/My Passport/FYP/chunks/"
 
-        audio_track = AudioSegment.from_file(f)
+    for filename in os.listdir(input_directory):
+        f = os.path.join(input_directory, filename)
+        if os.path.isfile(f):
+            # print(f)
+            # print(filename)
 
-        n = len(audio_track)
-        print(n)
-        interval = 30 * 1000  # pydub calculates in millisec
-        overlap = 15 * 1000
-        # Make chunks of one sec
-        counter = 1
-        # Export all of the individual chunks as wav files
+            audio_track = AudioSegment.from_file(f)
 
-        # Iterate from 0 to end of the file,
-        # with increment = interval
-        # why 2 n
-        # for i in range(0, 2 * n, interval):
-        for i in range(0, n * 2, interval):
-            if i == 0:
-                start = 0
-                end = interval
-                chunk = audio_track[start:end]
-            else:
-                start = end - overlap
-                end = start + interval
-                chunk = audio_track[start:end]
+            n = len(audio_track)
+            print(n)
+            interval = 30 * 1000  # pydub calculates in millisec
+            overlap = 15 * 1000
+            # Make chunks of one sec
+            counter = 1
+            # Export all of the individual chunks as wav files
 
-                if end >= n:
-                    continue
+            # Iterate from 0 to end of the file,
+            # with increment = interval
+            # why 2 n
+            # for i in range(0, 2 * n, interval):
+            for i in range(0, n * 2, interval):
+                if i == 0:
+                    start = 0
+                    end = interval
+                    chunk = audio_track[start:end]
+                else:
+                    start = end - overlap
+                    end = start + interval
+                    chunk = audio_track[start:end]
 
-            output_filename = filename + "_chunk" + str(counter) + '.ogg'
+                    if end >= n:
+                        continue
 
-            chunk.export(output_directory + output_filename, format="ogg")
-            print("Processing chunk " + str(counter) + ". Start = " + str(start) + " end = " + str(end))
-            counter = counter + 1
+                output_filename = filename + "_chunk_" + "%02d" % (counter,) + '.ogg'
+
+                chunk.export(output_directory + output_filename, format="ogg")
+                print("Processing chunk " + str(counter) + ". Start = " + str(start) + " end = " + str(end))
+                counter = counter + 1
