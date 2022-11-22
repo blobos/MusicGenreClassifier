b820d62174258858e21b4e9c6e486ccb5492158b
Aaron <aaronctsang@gmail.com>
2022-11-22 17:29:11 +0800

    Per track directories for chunks and spectograms

--- chunks2spectogram.py
+++ chunks2spectogram.py
@@ -5,23 +5,33 @@ import numpy as np
 import os
 
 def chunks2spectogram():
-    destination_directory = "/media/aaron/My Passport/FYP/spectograms/"
-    input_directory = '/media/aaron/My Passport/FYP/chunks/'
+    output_directory = "/media/aaron/My Passport/FYP/spectograms/"
+    chunk_directory = '/media/aaron/My Passport/FYP/chunks/'
 
     counter = 1
-    for filename in os.listdir(input_directory):
-        f = os.path.join(input_directory, filename)
-        if os.path.isfile(f):
-            y, sr = librosa.load(f)
+    for genre in os.listdir(chunk_directory):
+        chunk_genre_directory = os.path.join(chunk_directory, genre)
+        for subgenre in os.listdir(chunk_genre_directory):
+            chunk_subgenre_directory = os.path.join(chunk_genre_directory, subgenre)
+            for track in os.listdir(chunk_subgenre_directory):
+                chunk_track_directory = os.path.join(chunk_subgenre_directory, track)
+                for track_chunk in os.listdir(chunk_track_directory):
+                    track_chunk_path = os.path.join(chunk_track_directory, track_chunk)
+                    if os.path.isfile(track_chunk_path):
+                        y, sr = librosa.load(track_chunk_path)
 
-            n_mels = 128  # entire frequency spectrum split into 128
-            n_fft = 2048  # The amount of samples we are shifting after each fft (window size
-            hop_length = 512  # Short-time Fourier Transformation on our audio data
+                        n_mels = 128  # entire frequency spectrum split into 128
+                        n_fft = 2048  # The amount of samples we are shifting after each fft (window size
+                        hop_length = 512  # Short-time Fourier Transformation on our audio data
 
-            S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
-            S_DB = librosa.power_to_db(S, ref=np.max)
-            librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='mel')
-            plt.axis('off')
-            plt.savefig(destination_directory + filename + '.png', bbox_inches='tight', pad_inches=0)
-            print(str(counter) + ", " + filename)
-            counter = counter + 1
+                        S = librosa.feature.melspectrogram(y=y, sr=sr, n_fft=n_fft, hop_length=hop_length, n_mels=n_mels)
+                        S_DB = librosa.power_to_db(S, ref=np.max)
+                        librosa.display.specshow(S_DB, sr=sr, hop_length=hop_length, x_axis='time', y_axis='mel')
+                        plt.axis('off')
+
+                        spec_directory = output_directory + genre + "/" + subgenre + "/" + track + "/"
+                        if not os.path.exists(spec_directory):
+                            os.makedirs(spec_directory)
+                        plt.savefig(spec_directory + track_chunk + '.png', bbox_inches='tight', pad_inches=0)
+                        print(str(counter) + ", " + track_chunk)
+                        counter = counter + 1
--- splitter.py
+++ splitter.py
@@ -1,46 +1,65 @@
 from pydub import AudioSegment
 import os
 
-
 def splitter():
     input_directory = "/media/aaron/My Passport/FYP/sample_tracks/"
     output_directory = "/media/aaron/My Passport/FYP/chunks/"
 
-    for filename in os.listdir(input_directory):
-        f = os.path.join(input_directory, filename)
-        if os.path.isfile(f):
-            # print(f)
-            # print(filename)
+    for genre in os.listdir(input_directory):
+        # f = os.path.join(input_directory, filename)
+        # if os.path.isfile(f):
+        # print(f)
+        print("genre: ", genre)
+        genre_directory = os.path.join(input_directory, genre)
+        for subgenre in os.listdir(genre_directory):
+            print("subgenre: ", subgenre)
+            subgenre_directory = os.path.join(genre_directory, subgenre)
+            track_counter = 1
+            for track in os.listdir(subgenre_directory):
+                print("track: ", track)
+                track_path = os.path.join(subgenre_directory, track)
 
-            audio_track = AudioSegment.from_file(f)
+                audio_track = AudioSegment.from_file(track_path)
 
-            n = len(audio_track)
-            print(n)
-            interval = 30 * 1000  # pydub calculates in millisec
-            overlap = 15 * 1000
-            # Make chunks of one sec
-            counter = 1
-            # Export all of the individual chunks as wav files
+                n = len(audio_track)
+                print(n)
+                interval = 30 * 1000  # pydub calculates in millisec
+                overlap = 15 * 1000
+                # Make chunks of one sec
+                chunk_counter = 1
+                # Export all of the individual chunks as wav files
 
-            # Iterate from 0 to end of the file,
-            # with increment = interval
-            # why 2 n
-            # for i in range(0, 2 * n, interval):
-            for i in range(0, n * 2, interval):
-                if i == 0:
-                    start = 0
-                    end = interval
-                    chunk = audio_track[start:end]
-                else:
-                    start = end - overlap
-                    end = start + interval
-                    chunk = audio_track[start:end]
+                # Iterate from 0 to end of the file,
+                # with increment = interval
+                # why 2 n
+                # for i in range(0, 2 * n, interval):
+                for i in range(0, n * 2, interval):
+                    if i == 0:
+                        start = 0
+                        end = interval
+                        chunk = audio_track[start:end]
+                    else:
+                        start = end - overlap
+                        end = start + interval
+                        chunk = audio_track[start:end]
 
-                    if end >= n:
-                        continue
+                        if end >= n:
+                            continue
 
-                output_filename = filename + "_chunk_" + "%02d" % (counter,) + '.ogg'
 
-                chunk.export(output_directory + output_filename, format="ogg")
-                print("Processing chunk " + str(counter) + ". Start = " + str(start) + " end = " + str(end))
-                counter = counter + 1
+
+                    sort_name = subgenre + "_" + genre + "_" + str("%04d" % (track_counter,))
+                    # print(sort_name)
+                    output_filename = sort_name + "_chunk_" + "%02d" % (chunk_counter,) + '.ogg'
+                    # print(output_filename)
+                    chunk_directory = output_directory + genre + "/" + subgenre + "/" + sort_name + "/"
+                    # print(chunk_directory)
+                    # mode = 0o666
+                    if not os.path.exists(chunk_directory):
+                        os.makedirs(chunk_directory)
+
+                    chunk.export(chunk_directory + output_filename, format="ogg")
+                    # print("Processing chunk " + str(chunk_counter) + ". Start = " + str(start) + " end = " + str(end))
+                    chunk_counter = chunk_counter + 1
+
+                track_counter = track_counter + 1
