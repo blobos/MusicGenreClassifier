a492ad7036e24326c5b09c595fc4cf69fa56a3fd
blobos <vxc.shirou@gmail.com>
2023-01-11 03:03:22 +0800

    preprocessing finished

--- DataPreprocessing/splitter.py
+++ DataPreprocessing/splitter.py
@@ -1,151 +1,115 @@
 from pydub import AudioSegment
+from multiprocessing import Pool
 import os
 import csv
 
-# GENRES = [
-#     "Metal",
-#     "Rock",
-#     "Electronic",
-#     "Jazz",
-#     "Classical",
-#     "Pop"
-# ]
-#
-# SUBGENRES = ["Black", "Death", "Thrash",
-#              "Sludge", "Alternative", "Dreampop", "Indie", "Post", "Progressive", "Psychedelic",
-#              "Synthwave", "Techno", "House", "Trance"]
+chunk_length = 30 * 1000  # 30 seconds pydub calculates in millisec
+overlap = 15 * 1000  # 15 seconds
+bitrate = "128k"  # 128kbps
 
-SUBGENRES = ["Black Metal",
-             "Death Metal",
-             "Thrash Metal",
-             "Doom Metal",
-             "Alternative Rock",
-             "Dreampop (Rock)",
-             "Indie Rock",
-             "Post Rock",
-             "Progressive Rock",
-             "Psychedelic Rock",
-             "Folk Rock",
-             "Synthwave (Electronic)",
-             "Techno (Electronic)",
-             "House (Electronic)",
-             "Trance (Electronic)",
-             "Classical",
-             "Pop",
-             "Indie Pop"]
-#add Classical subgenres? Experimental subgenre? Classic Rock? Glam Rock?
 
-INTERVAL = 30 * 1000  # pydub calculates in millisec
-OVERLAP = 15 * 1000
-MAX_TRACK_LENGTH = 4 * 60 * 1000
+def split_audio(file, output_directory):
+    # print(file)
+    path_split = file.split("\\")
+    subgenre = path_split[0].split("/")[-1]
+    print("subgenre: ", subgenre)
 
-# to do:
-# audio adjustments?
-def track2chunks(input_directory, output_directory, csv_directory):
 
+    audio_track = AudioSegment.from_file(file)
+    subgenre_track_counter = path_split[1][:3]
+    start = 0
+    max_track_length = 4 * 60 * 1000
+    track_length = len(audio_track)
+
+    if track_length > max_track_length:
+        audio_track = audio_track[:max_track_length]
+
+    chunk_counter = 0
+    while start < track_length:
+        end = start + chunk_length
+        chunk = audio_track[start:end]
+
+        sort_name = subgenre + "_" + subgenre_track_counter
+        # black_metal_001
+        print("sortname: ",sort_name)
+        output_filename = sort_name + "_chunk_" + "%02d" % (chunk_counter,) + '.mp3'
+        print("filename: ", output_filename)
+        # black_metal_0001_chunk_01.mp3
+        chunk_directory = output_directory + subgenre + "/" + sort_name + "/"
+        # chunks/black_metal/black_metal_001/
+        print("directory: ",chunk_directory)
+        if not os.path.exists(chunk_directory):
+            os.makedirs(chunk_directory)
+
+        chunk.export(chunk_directory + output_filename, format="mp3", bitrate=bitrate)
+        # print("Processing chunk " + str(chunk_counter) + ". Start = " + str(start) + " end = " + str(end))
+        chunk_counter = chunk_counter + 1
+        start += chunk_length - overlap
+
+
+def add_to_CSV(input_directory, csv_directory):
     # CSV
-    header = ["Genre", "Genre Index", "Subgenre", "Subgenre Index", "Subgenre Track Number", "Filename"]
-    with open(csv_directory, "w") as csv_file:
+    header = ["Subgenre", "Subgenre Index", "Subgenre Track Number", "Total chunks for track", "Filename"]
+    with open(csv_directory, "w", newline='') as csv_file:
         writer = csv.writer(csv_file)
         writer.writerow(header)
 
-        for genre in os.listdir(input_directory):
+        for subgenre in os.listdir(input_directory):
             # f = os.path.join(input_directory, filename)
             # if os.path.isfile(f):
             # print(f)
-            print("genre: ", genre)
-            genre_directory = os.path.join(input_directory, genre)
-            for subgenre in os.listdir(genre_directory):
-                print("subgenre: ", subgenre)
-                subgenre_directory = os.path.join(genre_directory, subgenre)
-                #dictionary/mapping??
-                if subgenre == "black":
-                    subgenre_id = 0
-                elif subgenre == "death":
-                    subgenre_id = 1
-                elif subgenre == "thrash":
-                    subgenre_id = 2
-                elif subgenre == "doom":
-                    subgenre_id = 3
-                elif subgenre == "alternative":
-                    subgenre_id = 4
-                elif subgenre == "dreampop":
-                    subgenre_id = 5
-                elif subgenre == "indie":
-                    if genre == "rock":
-                        subgenre_id = 6
-                    elif genre == "pop":
-                        subgenre_id = 17
-                elif subgenre == "post":
-                    subgenre_id = 7
-                elif subgenre == "progressive":
-                    subgenre_id = 8
-                elif subgenre == "psychedelic":
-                    subgenre_id = 9
-                elif subgenre == "folk":
-                    subgenre_id = 10
-                elif subgenre == "synthwave": #drop microgenre?
-                    subgenre_id = 11
-                elif subgenre == "techno":
-                    subgenre_id = 12
-                elif subgenre == "house":
-                    subgenre_id = 13
-                elif subgenre == "trance":
-                    subgenre_id = 14
-                elif subgenre == "classical":
-                    subgenre_id = 15
-                elif subgenre == "pop":
-                    subgenre_id = 16
+            print("subgenre: ", subgenre)
+            subgenre_directory = os.path.join(input_directory, subgenre)
+            subgenre_map = {0: "alternative_rock", 1: "black_metal", 2: "death_metal", 3: "dreampop", 4: "heavy_metal",
+                            5: "house", 6: "indie_rock", 7: "post_rock", 8: "progressive_rock", 9: "punk_rock",
+                            10: "synthwave", 11: "techno", 12: "thrash_metal", 13: "trance"}
 
-                subgenre_track_counter = 1
-                for track in os.listdir(subgenre_directory):
-                    print("track: ", track)
-                    track_path = os.path.join(subgenre_directory, track)
-                    #load audio track
-                    audio_track = AudioSegment.from_file(track_path)
-                    # Split to Chunk
-                    track_length = len(audio_track)
-                    print(track_length)
+            for file in os.listdir(subgenre_directory):
+                # print("track: ", file)
+                track_path = os.path.join(subgenre_directory, file)
+                # load audio track to get total chunks
+                audio_track = AudioSegment.from_file(track_path)
+                track_length = len(audio_track)
+                total_subgenre_track_chunks = track_length // (chunk_length - overlap)  # 30 sec chunks with 15 overlap
+                # get subgenre id and subgenre track counter from filename
+                subgenre_id = get_key(subgenre_map, subgenre)
+                # print(subgenre_id, subgenre)
+                subgenre_track_counter = file[:3]
+                # write to csv
+                entry = [subgenre, subgenre_id, subgenre_track_counter, total_subgenre_track_chunks, file]
+                # print(entry)
+                writer.writerow(entry)
 
-                    # Make chunks of one sec
-                    chunk_counter = 1
-                    #write to csv
-                    total_subgenre_track_chunks = track_length/(INTERVAL-OVERLAP)#30 sec chunks with 15 overlap
-                    entry = [genre, subgenre, subgenre_id, subgenre_track_counter, total_subgenre_track_chunks, track]
-                    print(entry)
-                    writer.writerow(entry)
+def get_key(d, value):
+    for k, v in d.items():
+        if v == value:
+            return k
+    return None
 
-                    # Iterate from 0 to end of the file,
-                    # with increment = interval
-                    # why 2 n
-                    # for i in range(0, 2 * n, interval):
-                    if track_length > MAX_TRACK_LENGTH:
-                        track_length = MAX_TRACK_LENGTH
-                    for i in range(0, track_length * 2, INTERVAL):
-                        if i == 0:
-                            start = 0
-                            end = INTERVAL
-                            chunk = audio_track[start:end]
-                        else:
-                            start = end - OVERLAP
-                            end = start + INTERVAL
-                            chunk = audio_track[start:end]
+if __name__ == "__main__":
+    TRAIN_INPUT_DIRECTORY = "/FYP/data/train/tracks/"
+    TRAIN_OUTPUT_DIRECTORY = "/FYP/data/train/chunks/"
+    TRAIN_CSV_DIRECTORY = "/FYP/data/train/train_annotations.csv"
+    PREDICT_INPUT_DIRECTORY = "/FYP/data/predict/tracks/"
+    PREDICT_OUTPUT_DIRECTORY = "/FYP/data/predict/chunks/"
+    PREDICT_CSV_DIRECTORY = "/FYP/data/predict/predict_annotations.csv"
 
-                            if end >= track_length:
-                                continue
+    input_directory = TRAIN_INPUT_DIRECTORY
+    output_directory = TRAIN_OUTPUT_DIRECTORY
+    csv_directory = TRAIN_CSV_DIRECTORY
 
-                        sort_name = subgenre + "_" + genre + "_" + str("%04d" % (subgenre_track_counter,))
-                        # print(sort_name)
-                        output_filename = sort_name + "_chunk_" + "%02d" % (chunk_counter,) + '.ogg'
-                        # print(output_filename)
-                        chunk_directory = output_directory + genre + "/" + subgenre + "/" + sort_name + "/"
-                        # print(chunk_directory)
-                        # mode = 0o666
-                        if not os.path.exists(chunk_directory):
-                            os.makedirs(chunk_directory)
+    # make csv file
+    add_to_CSV(input_directory, csv_directory)
 
-                        chunk.export(chunk_directory + output_filename, format="ogg")
-                        # print("Processing chunk " + str(chunk_counter) + ". Start = " + str(start) + " end = " + str(end))
-                        chunk_counter = chunk_counter + 1
+    # split chunks
+    audio_files = []
+    # Get a list of all the subdirectories
+    subdirectories = [x[0] for x in os.walk(input_directory)]
+    # Get a list of all the audio files in the folder and subfolders
+    for subdir in subdirectories:
+        files = [os.path.join(subdir, f) for f in os.listdir(subdir) if f.endswith(".mp3")]
+        audio_files.extend(files)
 
-                    subgenre_track_counter = subgenre_track_counter + 1
+    # files = [f for f in os.listdir(input_directory)]
+    with Pool(processes=4) as pool:
+        pool.starmap(split_audio, [(file, output_directory) for file in audio_files])
--- ReadMe.md
+++ ReadMe.md
@@ -1,3 +1,5 @@
+Error: Writing CSV Memory Error
+Files to csv instead
 Error: input: NoneType in train.py and inference.py - not accessing file? but printing still gives tensor
 
 Aggregating inference: 
@@ -5,10 +7,12 @@ track ID or filepath enough?
 iterate:
 for all with same trackID?  or for total_subgenre_track_chunks with trackID?
 
-to do: 
-preprocessing parallel pooling
+to do:
+transfer learning
 make script for store multiple inferences 
 
 changes:
+Preprocessing Redone
+
 no conversion to mel spec - done in train/inference
  csv to format (filepath, track_chunk_#, total_track_chunks, classID, Class)
