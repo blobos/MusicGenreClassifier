7e41ba59b35fb139560f60030565e65982a8ba3a
blobos <vxc.shirou@gmail.com>
2023-02-17 15:15:40 +0800

    see ReadMe.md

--- FMA/genre_extractor.py
+++ FMA/genre_extractor.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import ast
 
-csv_path = "/FYP/data/FMA/tracks.csv"
+csv_path = "/home/student/Music/1/FYP/data/FMA/tracks.csv"
 # trackid, genre_top
 # techno:181, house 182, punk 25, shoegaze 359
 df = pd.read_csv(csv_path)
@@ -9,49 +9,53 @@ df = pd.read_csv(csv_path)
 # print(df.columns)
 techno_tracks = []
 house_tracks = []
-punk = []
-shoegaze = []
-# for line in df:
-# techno_tracks = df[df['genres'].apply(lambda x: 181 in x)['trackid']]
-# techno_tracks = df.loc[ast.literal_eval(df['genres']).apply(lambda x: 181 in x), 'trackid']
-# test = (ast.literal_eval(df.at[4, 'genres']))
-# print(test)
-# print(type(test))
-# print( 103 in test)
-temp = df['genres']
-print(df.columns.get_loc('genres'))
-# print(temp)
-# try:
-#     temp = df['genres'].apply(lambda x: ast.literal_eval(x))
-# except ValueError as e:
-temp = df[['trackid','genres']]
+punk_tracks = []
+shoegaze_tracks = []
 
-print(temp)
-print(temp['genres'])
-def safe_eval(x):
-    try:
-        if isinstance(x, str) and x.startswith('['):
-            return ast.literal_eval(x)
-        else:
-            return x
-    except (ValueError, SyntaxError) as e:
-        print(f"Error: {e} (value: {x})")
-        return None
-temp['genres'] = temp['genres'].apply(lambda x: safe_eval(x))
-# techno_tracks = temp.loc[mask, 'trackid']
+#clean up metadata
+temp = df.dropna(subset=['genres'])
+temp = temp[['trackid', 'genres']]
+temp = temp[temp['genres'].str.startswith('[')]
+# print("temp head: \n", temp.head())
+# print(temp['genres'][0])
+# print(type(temp['genres'][0]))
+
+#string to list from cell
+temp['genres'] = temp['genres'].apply(lambda x: ast.literal_eval(x))
+# print(temp.head())
+# print(temp['genres'][0])
+# print(type(temp['genres'][0]))
 
 
-# for index, row in df.iterrows():
-#     # if df.loc("genre_top") == "techno":
-#     if row["genre"] == "Techno":
-#         techno_tracks.append(row["trackid"])
-#         print(row["trackid"])
-#         # techno_tracks += df.loc("trackid")
+techno_tracks = temp[temp['genres'].apply(lambda x: 181 in x)]['trackid']
+house_tracks = temp[temp['genres'].apply(lambda x: 182 in x)]
+shoegaze_tracks = temp[temp['genres'].apply(lambda x: 359 in x)]
+punk_tracks = temp[temp['genres'].apply(lambda x: 25 in x)]
 
-print(techno_tracks)
 
-# if genre_top == techno
-# add to list
+# print("techno tracks:\n", techno_tracks)
 
-# for track in list:
-# copy to other folder
+#for tracks in techno copy to other folder
+
+track_directory = '/home/student/Music/1/FYP/data/FMA/fma_medium/'
+#for trackid in tracks
+print(type(techno_tracks))
+print(techno_tracks['0'])
+
+for trackid in techno_tracks:
+    trackid = '{:06d}'.format(trackid)
+
+#append leading 0's to track to get 6 digits
+#get first 3 digits for folder
+#copy track to other folder
+
+#folder structure
+#---fma_medium/
+#------/000/
+#----------/000002.mp3
+#...
+#----------/000998.mp3
+#------/001/
+#...
+#------/155/
+#----------/155064.mp3
