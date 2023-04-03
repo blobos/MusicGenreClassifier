fbb60082280229532dcb7b6892182341b12eac79
blobos <vxc.shirou@gmail.com>
2023-04-03 12:04:11 +0800

    predict on file selection, removed predict button, layout size alterations

--- Interface/UI_pyQT.py
+++ Interface/UI_pyQT.py
@@ -1,50 +1,69 @@
+from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QVBoxLayout, QFileDialog
 import sys
-from FYP.MusicGenreClassifier.Predict.inference_with_aggregate_single_track import combined
+from Predict.inference_with_aggregate_single_track import combined
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle('Classifier Interface')
+        self.setWindowTitle('Music Genre Classifier')
+
+        self.setMinimumSize(QSize(600, 600))
 
         self.file_path = ''
         self.prediction = ''
 
-        self.file_label = QLabel('No file selected.')
+        self.file_label = QLabel('Select audio file for classification')
         self.prediction_label = QLabel('')
 
         self.select_file_button = QPushButton('Select File')
         self.select_file_button.clicked.connect(self.select_file)
+        self.select_file_button.setFixedHeight(100)
 
-        self.predict_button = QPushButton('Predict')
-        self.predict_button.clicked.connect(self.predicting_label)
-        self.predict_button.clicked.connect(self.predict)
+
+        # self.predict_button = QPushButton('Predict')
+        # self.predict_button.clicked.connect(self.predicting_label)
+        # self.predict_button.clicked.connect(self.predict)
 
         layout = QVBoxLayout()
         layout.addWidget(self.file_label)
-        layout.addWidget(self.select_file_button)
-        layout.addWidget(self.predict_button)
         layout.addWidget(self.prediction_label)
+        layout.addWidget(self.select_file_button)
+        # layout.addWidget(self.predict_button)
+
 
         self.setLayout(layout)
 
+
     def select_file(self):
         file_dialog = QFileDialog()
-        self.file_path, _ = file_dialog.getOpenFileName(self, 'Open file', '', 'CSV Files (*.csv);;All Files (*)')
+        self.file_path, _ = file_dialog.getOpenFileName(self, 'Open file', '', 'MP3 Files (*.mp3);; '
+                                                                               'WAV Files (*.wav);;'
+                                                                               'FLAC files (*.flac);; '
+                                                                               'OGG files (*.ogg);; '
+                                                                               'OPUS files (*.opus'
+                                                                               ';;All Files (*)')
         if self.file_path:
-            self.file_label.setText(f'Selected file: {self.file_path}')
+            file_name = self.file_path.split("/")[-1]
+            self.file_label.setText(f'Predicting: {file_name}')
 
-    def predicting_label(self):
-        # TODO: add "predicting"
-        self.prediction_label.setText('Predicting...')
-    def predict(self):
-        MODEL_DIR = "/home/student/Music/1/FYP/MusicGenreClassifier/CNN/Model_Weights_Logs/vgg19/"
-        if self.file_path:
-            self.prediction = combined(self.file_path, MODEL_DIR)  # call your classifier function here
-            self.prediction_label.setText(f'Prediction: {self.prediction}')
-        else:
-            self.prediction_label.setText('Please select a file first.')
+        MODEL_DIR = "/home/cihe/Music/MusicGenreClassifier/CNN/Model_Weights_Logs/checkpoints_14_Epoch_no_val_improvement_in_10/"
+        self.prediction = combined(self.file_path, MODEL_DIR)  # call your classifier function here
+        self.prediction_label.setText(f'Prediction: {self.prediction}')
+
+    # def predicting_label(self):
+    #     # TODO: add "predicting"
+    #     self.prediction_label.setText('Predicting...')
+    #
+    # def predict(self):
+    #     MODEL_DIR = "/home/cihe/Music/MusicGenreClassifier/CNN/Model_Weights_Logs/checkpoints_14_Epoch_no_val_improvement_in_10/"
+    #     if self.file_path:
+    #         self.prediction = combined(self.file_path, MODEL_DIR)  # call your classifier function here
+    #         self.prediction_label.setText(f'Prediction: {self.prediction}')
+    #     else:
+    #         self.prediction_label.setText('Please select a file first.')
+
 
 
 if __name__ == '__main__':
