e9cb9acb7d6089abb227b32f05e75969ecca9d32
blobos <vxc.shirou@gmail.com>
2023-04-25 15:18:12 +0800

    progress bar added

--- Predict/CRNN/inference_voting_multitrack.py
+++ Predict/CRNN/inference_voting_multitrack.py
@@ -5,9 +5,10 @@ import torchaudio.transforms
 import pandas as pd
 from scipy.stats import mode
 
+from tqdm import tqdm
 sys.path.append("")
 from FYP.MusicGenreClassifier.CNN.Models.cnn_vgg19_leaky_relu_batchnorm_dropout import CNNNetwork
-from FYP.MusicGenreClassifier.CRNN.CRNN_no_cuda import CRNN
+from FYP.MusicGenreClassifier.CRNN.CRNN_test.CRNN_biLSTM import NetworkModel
 # from FYP.MusicGenreClassifier.CNN.train_with_validation_binary import SAMPLE_RATE, NUM_SAMPLES, N_FFT, HOP_LENGTH, N_MELS
 # from FYP.MusicGenreClassifier.CNN.Models.cnn_vgg16 import CNNNetwork
 from FYP.MusicGenreClassifier.Predict.song_level_all_test_tracks.datasetmelspecprep_for_multitrack import DatasetMelSpecPrep
@@ -32,25 +33,30 @@ def predict(model, input):
     # print("prediction for ")
     model.eval()
     with torch.no_grad():
-        predictions = model(input[0].unsqueeze_(0))
+        predictions = model(input[0].unsqueeze_(0)) #why unsqueeze
         # Tensor (1, 10) -> [ [0.1, 0.01, ..., 0.6] ]
-
+        # print("predict() predictions:", predictions)
         # print(predictions[0])
 
         expected_label = input[1]
         path = input[2]
         # print(expected_label, path)
-    return path.split("/")[-1], expected_label, predictions[0]
+    return path.split("/")[-1], expected_label, predictions
 
 
-def prediction2df(predict_list, dmsp, cnn):
+def prediction2df(predict_list, dmsp, network_model):
     # for each prediction of input/dmsp
-    for i in range(0, len(dmsp)):
-        input = dmsp[i]
-        path, expected, prediction = predict(cnn, input)
-        print(prediction)
-        print(prediction.shape)
-        print("prediction argmax:", prediction.argmax(0).item())
+    # print("dmsp length:", len(dmsp))
+    # for i in range(0, len(dmsp)):
+    #     input = dmsp[i] #(tensor, expected, path)
+    for input in tqdm(dmsp, desc="Predicting chunks", unit='chunks'):
+        # print("input:", input, type(input))
+        # print("waveform:", input[0].shape)
+        path, expected, prediction = predict(network_model, input)
+        # print(path, expected, prediction)
+        #prediction = tensor of 10 tensors
+        # print("prediction:", prediction, prediction.size(), type(prediction))
+        # print("prediction argmax:", prediction.argmax(0).item())
         # row = pd.Series({'path': path, 'expected': expected, 'predict': prediction})
         # print("row", row)
         row = [path, expected, prediction.argmax(0).item()]
@@ -58,8 +64,10 @@ def prediction2df(predict_list, dmsp, cnn):
     return predict_list
 
 
+
 def prediction_vote(group):
     # Use the mode function to vote for the prediction
+    #todo: comment why [0][0]
     voted_prediction = mode(group["prediction"])[0][0]
     return voted_prediction
 
@@ -77,13 +85,14 @@ def inference_voting_multitrack(model_path, parameters, prediction_output_csv_di
         # Get audio sample parameters from parameters.txt for mel spectrogram transformations
         with open(parameters, "r") as f:
             line = f.readlines()
-            print(line[0])
-            SAMPLE_RATE = int(line[5].split()[-1])
-            NUM_SAMPLES = SAMPLE_RATE
-            N_FFT = int(line[6].split()[-1])
-            HOP_LENGTH = int(line[7].split()[-1])
-            N_MELS = int(line[8].split()[-1])
+            print(line[5])
+            SAMPLE_RATE = int(line[6].split()[-1])
+            NUM_SAMPLES = int(line[7].split()[-1])
+            N_FFT = int(line[8].split()[-1])
+            HOP_LENGTH = int(line[9].split()[-1])
+            N_MELS = int(line[10].split()[-1])
             print(f"Sample Rate: {SAMPLE_RATE}\n"
+                  f"Number of samples: {NUM_SAMPLES}"
                   f"N_FFT: {N_FFT}\n"
                   f"Hop length: {HOP_LENGTH}\n"
                   f"N_MELS: {N_MELS}")
@@ -98,9 +107,9 @@ def inference_voting_multitrack(model_path, parameters, prediction_output_csv_di
 
             # audiofile_annotations = "/home/student/Music/1/FYP/data/test_annotations.csv"
             # audiofile_dir = "/home/student/Music/1/FYP/data/test/chunks"
-            cnn = CRNN()
+            network_model = NetworkModel()
             state_dict = torch.load(model_path, map_location=torch.device('cpu'))
-            cnn.load_state_dict(state_dict)
+            network_model.load_state_dict(state_dict)
 
             dmsp = DatasetMelSpecPrep(audiofile_annotations,
                                       audiofile_dir,
@@ -110,7 +119,7 @@ def inference_voting_multitrack(model_path, parameters, prediction_output_csv_di
                                       "cpu")
 
             chunk_prediction_list = []
-            prediction2df(chunk_prediction_list, dmsp, cnn)
+            prediction2df(chunk_prediction_list, dmsp, network_model)
 
             chunk_predictions = pd.DataFrame(chunk_prediction_list, columns=['path', 'expected', 'prediction'])
             chunk_predictions_csv_path = prediction_output_csv_dir + 'chunk_predictions.csv'
@@ -135,19 +144,21 @@ def inference_voting_multitrack(model_path, parameters, prediction_output_csv_di
 if __name__ == "__main__":
 
     # Replace model_path and parameters.txt
-    checkpoint_dir = "../../CNN/Model_Weights_Logs/simple_vgg16_test/"
+    checkpoint_dir = "../../CRNN/CRNN_test/"
     model_path = checkpoint_dir + "lowest_val_loss.pth"
     parameters = checkpoint_dir + "parameters.txt"
 
     # Get audio sample parameters from parameters.txt for mel spectrogram transformations
     with open(parameters, "r") as f:
         line = f.readlines()
-        SAMPLE_RATE = int(line[5].split()[-1])
-        NUM_SAMPLES = SAMPLE_RATE
-        N_FFT = int(line[6].split()[-1])
-        HOP_LENGTH = int(line[7].split()[-1])
-        N_MELS = int(line[8].split()[-1])
+        print(line[5])
+        SAMPLE_RATE = int(line[6].split()[-1])
+        NUM_SAMPLES = int(line[7].split()[-1])
+        N_FFT = int(line[8].split()[-1])
+        HOP_LENGTH = int(line[9].split()[-1])
+        N_MELS = int(line[10].split()[-1])
         print(f"Sample Rate: {SAMPLE_RATE}\n"
+              f"Number of samples: {NUM_SAMPLES}"
               f"N_FFT: {N_FFT}\n"
               f"Hop length: {HOP_LENGTH}\n"
               f"N_MELS: {N_MELS}")
@@ -162,9 +173,9 @@ if __name__ == "__main__":
 
     audiofile_annotations = "/home/student/Music/1/FYP/data/test_annotations.csv"
     audiofile_dir = "/home/student/Music/1/FYP/data/test/chunks"
-    cnn = CRNN()
+    network_model = NetworkModel()
     state_dict = torch.load(model_path)
-    cnn.load_state_dict(state_dict)
+    network_model.load_state_dict(state_dict)
 
     dmsp = DatasetMelSpecPrep(audiofile_annotations,
                               audiofile_dir,
@@ -174,7 +185,7 @@ if __name__ == "__main__":
                               "cpu")
 
     chunk_prediction_list = []
-    prediction2df(chunk_prediction_list, dmsp, cnn)
+    prediction2df(chunk_prediction_list, dmsp, network_model)
 
     chunk_predictions = pd.DataFrame(chunk_prediction_list, columns=['path', 'expected', 'prediction'])
     chunk_predictions_csv_path = checkpoint_dir + 'chunk_predictions.csv'
